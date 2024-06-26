abc693954fa430564117b86b145c9746fc1946ab
Filipe Alves <filipe@0xcarbon.org>
2024-06-26 16:53:25 -0300

    wip

--- rust-c-tss/src/dkls23/protocols/dkg.rs
+++ rust-c-tss/src/dkls23/protocols/dkg.rs
@@ -1,7 +1,9 @@
 use crate::dkls23::utilities::c_types::{
-    CBroadcastDerivationPhase2to4, CPhase2Out, CProofCommitment, CScalar,
-    CScalarVec, CSessionData, CTransmitInitZeroSharePhase2to4Vec,
-    CUniqueKeepDerivationPhase2to3,
+    CBroadcastDerivationPhase2to4, CBroadcastDerivationPhase3to4,
+    CKeepInitMulPhase3to4BTreeMap, CKeepInitZeroSharePhase3to4, CPhase2Out,
+    CPhase3Out, CProofCommitment, CScalar, CScalarVec, CSessionData,
+    CTransmitInitMulPhase3to4Vec, CTransmitInitZeroSharePhase2to4Vec,
+    CTransmitInitZeroSharePhase3to4Vec, CUniqueKeepDerivationPhase2to3,
 };
 use dkls23::protocols::dkg;
 
@@ -43,6 +45,35 @@ pub extern "C" fn dkls_phase2(
         proof_commitment: c_proof_commitment,
         zero_transmit: c_zero_transmit_vec,
         bip_keep: c_bip_keep,
+        bip_broadcast: c_bip_broadcast,
+    }
+}
+
+#[no_mangle]
+pub extern "C" fn dkls_phase3(
+    c_session: &CSessionData,
+    c_zero_kept: &CKeepInitZeroSharePhase2to3BTreeMap,
+    c_bip_kept: &CUniqueKeepDerivationPhase2to3,
+) -> CPhase3Out {
+    let session = c_session.to_session();
+    let zero_kept = c_zero_kept.to_inner();
+    let bip_kept = c_bip_kept.to_inner();
+
+    let (zero_keep, zero_transmit, mul_keep, mul_transmit, bip_broadcast) =
+        dkg::phase3(&session, &zero_kept, &bip_kept);
+
+    //let c_zero_keep = CKeepInitZeroSharePhase3to4BTreeMap::from(&zero_keep);
+    let c_zero_transmit =
+        CTransmitInitZeroSharePhase3to4Vec::from(&zero_transmit);
+    //let c_mul_keep = CKeepInitMulPhase3to4BTreeMap::from(&mul_keep);
+    let c_mul_transmit = CTransmitInitMulPhase3to4Vec::from(&mul_transmit);
+    let c_bip_broadcast = CBroadcastDerivationPhase3to4::from(&bip_broadcast);
+
+    CPhase3Out {
+        //zero_keep: c_zero_keep,
+        zero_transmit: c_zero_transmit,
+        //mul_keep: c_mul_keep,
+        mul_transmit: c_mul_transmit,
         bip_broadcast: c_bip_broadcast,
     }
 }
