68e1e6d174c2868418cd525719f53519e6d7fa03
Filipe Alves <filipe@0xcarbon.org>
2024-06-25 22:38:42 -0300

    feat(dkg): use convertions on c_types to return CPhase2Out

--- rust-c-tss/src/dkls23/protocols/dkg.rs
+++ rust-c-tss/src/dkls23/protocols/dkg.rs
@@ -1,6 +1,7 @@
 use dkls23::protocols::dkg;
 use crate::dkls23::utilities::c_types::{
-    CSessionData, CScalarVec,
+    CBroadcastDerivationPhase2to4, CPhase2Out, CProofCommitment, CSessionData,
+    CScalar, CScalarVec, CTransmitInitZeroSharePhase2to4Vec, CUniqueKeepDerivationPhase2to3,
 };
 
 #[no_mangle]
@@ -15,7 +16,7 @@ pub extern "C" fn dkls_phase1(session: &CSessionData) -> CScalarVec {
 pub extern "C" fn dkls_phase2(
     session: &CSessionData,
     c_poly_fragments: &CScalarVec
-) {
+) -> CPhase2Out {
     let session_data = CSessionData::to_session(session);
     let poly_fragments = c_poly_fragments.to_vec();
 
@@ -27,4 +28,19 @@ pub extern "C" fn dkls_phase2(
         bip_keep,
         bip_broadcast,
     ) = dkg::phase2(&session_data, poly_fragments.as_slice());
+
+    let c_poly_point = CScalar::from(&poly_point);
+    let c_proof_commitment = CProofCommitment::from(&proof_commitment);
+    // let zero_keep = BTreeMap
+    let c_zero_transmit_vec = CTransmitInitZeroSharePhase2to4Vec::from(&zero_transmit);
+    let c_bip_keep = CUniqueKeepDerivationPhase2to3::from(&bip_keep);
+    let c_bip_broadcast = CBroadcastDerivationPhase2to4::from(&bip_broadcast);
+
+    CPhase2Out {
+        poly_point: c_poly_point,
+        proof_commitment: c_proof_commitment,
+        zero_transmit: c_zero_transmit_vec,
+        bip_keep: c_bip_keep,
+        bip_broadcast: c_bip_broadcast,
+    }
 }
