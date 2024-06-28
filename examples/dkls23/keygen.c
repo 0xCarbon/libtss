ce8257627b8958813c7034b2f49acc0ccfddcb31
Filipe Alves <filipe@0xcarbon.org>
2024-06-28 12:55:37 -0300

    feat(examples): use test_phase1 on main

--- examples/dkls23/keygen.c
+++ examples/dkls23/keygen.c
@@ -1,14 +1,13 @@
-#include <time.h>
-#include "utils.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include "tests.h"
 
 int main() {
-    time_t t;
-    srand(time(&t));
-
-    SessionData *session_data_1 = create_session(1, 2, 2, 32);
-    ScalarVec scalars = dkls_phase1(session_data_1);
-    //print_scalar_vec(&scalars);
-
-    Phase2Out phase2_out = dkls_phase2(session_data_1, &scalars);
-    print_phase_2(&phase2_out);
+    bool success = test_phase1();
+    if (success) {
+        printf("phase 1: passed");
+    } else {
+        printf("phase 1 failed");
+        exit(1);
+    }
 }
