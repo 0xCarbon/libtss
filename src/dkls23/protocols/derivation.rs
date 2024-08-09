55fb887776909881958b6d565437e6b3c22f3989
Filipe Alves <filipe@0xcarbon.org>
2024-08-09 00:55:33 -0300

    refactor(derivation): rename to derive_from_path

--- src/dkls23/protocols/derivation.rs
+++ src/dkls23/protocols/derivation.rs
@@ -5,33 +5,33 @@ use std::os::raw::c_char;
 
 // Derivation structs
 #[derive(Deserialize, Serialize)]
-pub struct DerivationIn {
+pub struct DeriveFromPathIn {
     pub data: DerivData,
     pub path: String,
 }
 
 #[derive(Deserialize, Serialize)]
-pub struct DerivationOut {
+pub struct DeriveFromPathOut {
     pub data: DerivData,
 }
 
-impl CJson for DerivationIn {}
-impl CJson for DerivationOut {}
+impl CJson for DeriveFromPathIn {}
+impl CJson for DeriveFromPathOut {}
 
 #[no_mangle]
-pub extern "C" fn dkls_derivation(
-    derivation_json_in: *const c_char,
+pub extern "C" fn dkls_derive_from_path(
+    derive_json_in: *const c_char,
 ) -> *const c_char {
-    let derivation_in: DerivationIn =
-        DerivationIn::from_json(derivation_json_in);
-    match derivation_in
+    let derive_from_path_in: DeriveFromPathIn =
+        DeriveFromPathIn::from_json(derive_json_in);
+    match derive_from_path_in
         .data
-        .derive_from_path(derivation_in.path.as_str())
+        .derive_from_path(derive_from_path_in.path.as_str())
     {
         Err(error) => {
             panic!("Derivation error: {:?}", error);
         }
 
-        Ok(data) => DerivationOut { data }.to_json(),
+        Ok(data) => DeriveFromPathOut { data }.to_json(),
     }
 }
